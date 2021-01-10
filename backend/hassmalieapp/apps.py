678b55ed832389795741fffd312a6ff05179393e
Alaa Yahia <alaa.yahia.1995@gmail.com>
2021-01-10 15:46:39 +0200

    first commit

---
+++ backend/hassmalieapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HassmalieappConfig(AppConfig):
+    name = 'hassmalieapp'
---
+++ reacthassmalie/src/components/Navbar.js
@@ -0,0 +1,60 @@
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logout } from '../actions/auth';
+
+const Navbar = ({ logout, isAuthenticated }) => {
+//
+//    const logout_user = () => {
+//        logout();
+//        setRedirect(true);
+//    };
+
+    const guestLinks = () => (
+        <Fragment>
+            <li className='nav-item'>
+                <Link className='nav-link' to='/login'>Login</Link>
+            </li>
+            <li className='nav-item'>
+                <Link className='nav-link' to='/signup'>Sign Up</Link>
+            </li>
+        </Fragment>
+    );
+
+    const authLinks = () => (
+        <li className='nav-item'>
+            <a className='nav-link' href='#!' onClick={logout}>Logout</a>
+        </li>
+    );
+
+    return (
+            <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+                <Link className='navbar-brand' to='/'>Auth System</Link>
+                <button
+                    className='navbar-toggler'
+                    type='button'
+                    data-toggle='collapse'
+                    data-target='#navbarNav'
+                    aria-controls='navbarNav'
+                    aria-expanded='false'
+                    aria-label='Toggle navigation'
+                >
+                    <span className='navbar-toggler-icon'></span>
+                </button>
+                <div className='collapse navbar-collapse' id='navbarNav'>
+                    <ul className='navbar-nav'>
+                        <li className='nav-item active'>
+                            <Link className='nav-link' to='/'>Home <span className='sr-only'>(current)</span></Link>
+                        </li>
+                        {isAuthenticated ? authLinks() : guestLinks()}
+                    </ul>
+                </div>
+            </nav>
+    );
+};
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { logout })(Navbar);
